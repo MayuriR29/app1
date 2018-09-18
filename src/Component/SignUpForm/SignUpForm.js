bb17af3eca824580e5bdf7db08883611b1442215
MayuriR29 <mayuri.gr@gmail.com>
2018-09-18 18:53:32 +0800

    Implementation of sign/signup forms

---
+++ src/Component/SignUpForm/SignUpForm.js
@@ -0,0 +1,104 @@
+import React, { Component } from "react";
+import { Form, Input,Select,Button } from "antd";
+
+const FormItem = Form.Item;
+const Option = Select.Option;
+
+class WrappedRegistrationForm extends Component {
+    
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 }
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 }
+      }
+    };
+    const tailFormItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0
+        },
+        sm: {
+          span: 16,
+          offset: 8
+        }
+      }
+    };
+    const prefixSelector = getFieldDecorator('prefix', {
+        initialValue: '86',
+      })(
+        <Select style={{ width: 70 }}>
+          <Option value="86">+86</Option>
+          <Option value="87">+87</Option>
+        </Select>
+      );
+    return (
+      <div>
+        <Form onSubmit={this.handleSubmit}>
+          <FormItem {...formItemLayout} label="E-mail">
+            {getFieldDecorator("email", {
+              rules: [
+                {
+                  type: "email",
+                  message: "The input is not valid E-mail!"
+                },
+                {
+                  required: true,
+                  message: "Please input your E-mail!"
+                }
+              ]
+            })(<Input />)}
+          </FormItem>
+          <FormItem {...formItemLayout} label="Password">
+            {getFieldDecorator("password", {
+              rules: [
+                {
+                  required: true,
+                  message: "Please input your password!"
+                },
+                {
+                  validator: this.validateToNextPassword
+                }
+              ]
+            })(<Input type="password" />)}
+          </FormItem>
+          <FormItem {...formItemLayout} label="Confirm Password">
+            {getFieldDecorator("confirm", {
+              rules: [
+                {
+                  required: true,
+                  message: "Please confirm your password!"
+                },
+                {
+                  validator: this.compareToFirstPassword
+                }
+              ]
+            })(<Input type="password" onBlur={this.handleConfirmBlur} />)}
+          </FormItem>
+          <FormItem {...formItemLayout} label="Phone Number">
+            {getFieldDecorator("phone", {
+              rules: [
+                { required: true, message: "Please input your phone number!" }
+              ]
+            })(
+              <Input addonBefore={prefixSelector} style={{ width: "100%" }} />
+            )}
+          </FormItem>
+          <FormItem {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit">
+              Register
+            </Button>
+          </FormItem>
+        </Form>
+      </div>
+    );
+  }
+}
+const SignUpForm = Form.create()(WrappedRegistrationForm);
+export default SignUpForm;
