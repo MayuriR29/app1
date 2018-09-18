62a46534613912bbd03925779725130722d86e80
MayuriR29 <mayuri.gr@gmail.com>
2018-09-18 18:52:34 +0800

    updated test description

--- src/Component/App/App.test.js
+++ src/Component/App/App.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import ShallowRenderer from 'react-test-renderer/shallow'
 
-it('shpuld render snapshot correctly', () => {
+it('should render snapshot correctly', () => {
   const renderer=new ShallowRenderer();
   renderer.render(<App/>);
   const output=renderer.getRenderOutput();
